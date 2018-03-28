037e24231b67cd8538813d32f82c5bb6236db97f
orkhydea <orkhydea33@gmail.com>
2018-03-28 11:17:06 -0600

    se agregan tres funciones en js para lograr la funcionalidad del problema a resolver

--- js/app.js
+++ js/app.js
@@ -1,1 +1,30 @@
-console.log("cualquier cosa");
+var scores = {}; // va a llevar el registro de los jugadores
+console.log(scores);
+//
+function createPlayer(object, name) {
+  object[name.toLowerCase()] = 0;
+console.log(object);
+}
+
+function addPoints(name, points) {
+  scores[name.toLowerCase()] += points;
+  console.log(scores);
+}
+
+function printAllPoints() {
+var arrayPlayers = object.keys(scores);
+var resultados = '';
+for (var k = 0; k < arrayPlayers.length; k++) {
+  if (k == arrayPlayers - 1) {//se verifica que estemos en el ultimo index para quitar la coma que separa los elementos
+  } else {
+    resultados += arrayPlayers[k] +': ' + scores[arrayPlayers[k]]+ ', ';
+  }
+}
+return resultados;
+}
+
+createPlayer(scores, 'Ana');
+addPoints('ana', 20);
+addPoints('ana', 40);
+createPlayer(scores, 'Silvana', 50);
+console.log(printAllPoints());
