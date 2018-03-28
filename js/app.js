87a3da7ae14edaea1b95a074b45412fccfee0b3e
orkhydea <orkhydea33@gmail.com>
2018-03-28 11:43:17 -0600

    se agregan tres funciones en js para lograr la funcionalidad del problema a resolver

--- js/app.js
+++ js/app.js
@@ -12,12 +12,14 @@ function addPoints(name, points) {
 }
 
 function printAllPoints() {
-var arrayPlayers = object.keys(scores);
+var arrayPlayers = Object.keys (scores);
 var resultados = '';
 for (var k = 0; k < arrayPlayers.length; k++) {
-  if (k == arrayPlayers - 1) {//se verifica que estemos en el ultimo index para quitar la coma que separa los elementos
+  if (k == arrayPlayers.length - 1) {//se verifica que estemos en el ultimo index para quitar la coma que separa los elementos
+    resultados += arrayPlayers[k] +': ' + scores[arrayPlayers[k]]+ '. ';
   } else {
     resultados += arrayPlayers[k] +': ' + scores[arrayPlayers[k]]+ ', ';
+    console.log(resultados);
   }
 }
 return resultados;
@@ -26,5 +28,6 @@ return resultados;
 createPlayer(scores, 'Ana');
 addPoints('ana', 20);
 addPoints('ana', 40);
-createPlayer(scores, 'Silvana', 50);
+createPlayer(scores, 'Silvana');
+addPoints('silvana', 50);
 console.log(printAllPoints());
